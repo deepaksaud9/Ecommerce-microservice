d75387e3b26f2e58c8b53a2d7e19018f7a8ec0f3
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-02-11 15:14:00 +0545

    feat| order service/ order business logic developed

---
+++ Backend-java/order-service/order-service/src/main/java/com/ecommerce/order/orderservice/model/Order.java
@@ -0,0 +1,38 @@
+package com.ecommerce.order.orderservice.model;
+
+import com.ecom.common.model.Auditable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_table")
+public class Order extends Auditable<String> implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "order_description")
+    private String orderDescription;
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "order_Quantity")
+    private Integer quantity;
+
+    @Column(name = "order_price")
+    private BigDecimal orderPrice;
+
+//    private String userDetails;
+
+}
